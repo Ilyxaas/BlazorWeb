c73db4916cdec2e64c37f3e752bb9eaf56ba6128
Ilyxaas <82675311+Ilyxaas@users.noreply.github.com>
2024-03-12 13:40:51 +0400

    Client

---
+++ AuthorizationForms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorProject.Client
+{
+	public class AuthorizationForms
+	{
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string? login { get; set; }
+
+
+        [StringLength(20, MinimumLength = 8)]
+        [Required]
+        public string? Password { get; set; }
+
+        public AuthorizationForms()
+		{
+
+		}
+	}
+}
+
---
+++ RegistrationForms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorProject.Client
+{
+	public enum Gender
+	{
+		Man = 1,
+        Woman = 2
+    }
+
+
+    public class RegistrationForms
+	{
+        public RegistrationForms()
+        {
+
+        }
+        
+        public string? ID { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        [Required]        
+        public string? email { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 6)]
+        public string? login { get; set; }
+
+        [StringLength(20, MinimumLength = 8)]
+        [Required]
+        public string? Password { get; set; }
+
+        [StringLength(20, MinimumLength = 8)]
+        [Required]
+        public string? PasswordRepetition { get; set; }
+
+        [Range(1, 100)]
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public DateTime Time { get; set; }
+
+    }
+}
+
